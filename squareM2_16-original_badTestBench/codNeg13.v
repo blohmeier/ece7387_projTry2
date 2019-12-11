b82a79e26d4f807e1006f525ca68b4de553438da
Brett Lohmeier <brett.lohmeier@gmail.com>
2019-12-11 12:46:28 -0600

    Create codNeg13.v

---
+++ squareM2_16-original_badTestBench/codNeg13.v
@@ -0,0 +1,21 @@
+`timescale 1ns / 1ps
+/*
+* Negates the 13-bit input i if
+* c is 1.
+* Input: i
+* Output: o
+*/
+module codNeg13 (i, c, o);
+input[12:0] i;
+input c;
+output[12:0] o;
+reg[12:0] o;
+integer j;
+always @ (i or c)
+begin
+for (j = 0; j < 13; j = j + 1)
+begin
+o[j] = c ^ i[j];
+end
+end
+endmodule
